c163ddf98745fdcb3fb349f943990ae7ef7984bb
white <white-johny@mail.ru>
2024-03-18 19:01:20 +0700

    %2==0

---
+++ Task06/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на 2 без остатка)
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 %2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
