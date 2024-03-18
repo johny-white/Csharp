1ec826f10683a461af0ccf2f1cfb3d058133f79c
white <white-johny@mail.ru>
2024-03-18 20:10:03 +0700

    Task04

---
+++ Seminar2/Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1=number;
+int number2=1;
+while (number1>=10)
+{
+    number1/=10;
+    number2*=10;
+}
+while (number2>1)
+{
+   Console.Write(number/number2%10 +", ");
+   number2/=10;
+ }
+Console.WriteLine(number%10);
