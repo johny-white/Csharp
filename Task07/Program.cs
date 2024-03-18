bc882d3f04809a9cffb279e80ed7eb4c9211aa82
white <white-johny@mail.ru>
2024-03-18 17:35:33 +0700

    %10

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,3 +1,13 @@
 ﻿/* Напишите программу, которая принимает на вход трёхзначное число 
 и на выходе показывает последнюю цифру этого числа
 */
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
